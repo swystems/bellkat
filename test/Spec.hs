b0b768ae0d2722a4ec39f9baee5d950061d13c0f
Pavel Chuprikov <pschuprikov@gmail.com>
2022-12-09 16:19:46 +0100

    add more tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,11 +1,18 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE OverloadedLists #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module Spec where
 
 import QNKAT.Definitions
 import QNKAT.UnorderedTree
 import QNKAT.Test
+import QNKAT.Drawing
+
+import Data.Set (Set)
+import Data.Monoid (Sum(..))
+import Control.Monad (unless)
+import GHC.Exts
 
 import Test.Hspec
 import Test.Hspec.QuickCheck
@@ -16,16 +23,26 @@ main = hspec $ do
     describe "distill" $ do
         it "should drop sometimes" $
             applyPolicy (Distill ("A", "B")) [Node ("A" :~: "B") [], Node ("A" :~: "B") []] 
-                `shouldSatisfy` any (null . getForest)
+                `historiesShouldSatisfy` any (null . getForest)
     describe "transmit" $ do
         it "should transmit" $
             applyPolicy (Transmit "A" ("A", "R[AB]")) [Node ("A" :~: "A") []]
-                `shouldSatisfy` any (any (hasRoot ("A" :~: "R[AB]")) . getForest)
+                `historiesShouldSatisfy` any (any (hasRoot ("A" :~: "R[AB]")) . getForest)
         it "should transmit two" $
             applyPolicy 
                     (Transmit "A" ("A", "R[AB]") <||> Transmit "B" ("B", "R[AB]")) 
                     [Node ("A" :~: "A") [], Node ("B" :~: "B") []]
-                `shouldSatisfy` any (any (hasRoot ("A" :~: "R[AB]")) . getForest)
+                `historiesShouldSatisfy` any (any (hasRoot ("A" :~: "R[AB]")) . getForest)
+        it "should transmit one out of two" $
+            applyPolicy
+                   (Transmit "A" ("A", "R[AB]")) 
+                    [Node ("A" :~: "A") [], Node ("A" :~: "A") []]
+                `historiesShouldSatisfy` all ((== 2) . length . getForest)
+        it "should transmit two out of three" $
+            applyPolicy
+                   (Transmit "A" ("A", "R[AB]") <||> Transmit "A" ("A", "R[AB]")) 
+                   (fromList . replicate 3 $ Node ("A" :~: "A") [])
+                `historiesShouldSatisfy` all ((== 3) . length . getForest)
     describe "choose" $ do
         it "should choose nothing" $
             choose 0 ["A"] `shouldBe` [chooseNoneOf ["A"]]
@@ -33,6 +50,8 @@ main = hspec $ do
             choose 1 ["A"] `shouldBe` [chooseAll ["A"]]
         it "should choose two" $
             choose 2 ["A" ,"B"] `shouldBe` [chooseAll ["A", "B"]]
+        prop "should preserve length" $
+            \n (xs :: [Int]) -> all (isPartial (Sum $ length xs) . fmap (Sum . length)) (choose n xs)
     describe "bell pair" $ do
         it "A~B == B~A" $
             ("A" :~: "B") `shouldBe` ("B" :~: "A")
@@ -46,3 +65,17 @@ main = hspec $ do
     describe "chooseHistories" $ do
         prop "should not be commutative" $ expectFailure $
             \x y h -> chooseHistories x y h == chooseHistories y x h
+    describe "findSubHistoryND" $ do
+        prop "should return partial" $ 
+            \ps h -> all (isPartial h) (findSubHistoryND ps h)
+    describe "findTreeRootsND" $ do
+        prop "should return partial" $ 
+            \ps h -> all (isPartial h) (findTreeRootsND ps h)
+
+isPartial :: (Eq a, Semigroup a) => a -> Partial a -> Bool
+isPartial x partialX = x == (chosen partialX <> rest partialX)
+
+historiesShouldSatisfy :: Set History -> (Set History -> Bool) -> Expectation
+historiesShouldSatisfy h f = 
+    unless (f h) $ expectationFailure $ "nope:\n" <> drawHistoriesText h
+
