905cbf1f01b595d6204ceadfeb266c845e92d20f
Pavel Chuprikov <pschuprikov@gmail.com>
2024-03-08 09:09:28 +0100

    add validity check to the paper spec

--- test/paper/SpecPaper.hs
+++ test/paper/SpecPaper.hs
@@ -1,14 +1,22 @@
 {-# LANGUAGE OverloadedLists #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TypeApplications #-}
 
 module SpecPaper where
 
 import Data.Foldable (toList)
 import BellKAT.DSL
 import BellKAT.Definitions hiding (test, (<.>))
+import Data.Maybe
+import qualified Data.Map.Strict as Map
+import Data.Map.Strict (Map)
 
 type PaperPolicy = NormalWithTests StarPolicy FreeTest (Maybe ())
+
+countQubitsAtLocation :: Location -> TaggedBellPairs tag -> Int
+countQubitsAtLocation l = length . filter (hasLocation l . bellPair) . toList
+
+memoryBounds :: Map Location Int -> TaggedBellPairs tag -> Bool
+memoryBounds bounds bps = all (\(l,k) -> countQubitsAtLocation l bps <= k) $ Map.toList bounds
 
 main :: IO ()
 main =
@@ -32,6 +40,9 @@ main =
         pad = pd <> star bpd <> test ("A" ~~? "D") 
         ped = pd' <> star bpd' <> test ("E" ~~? "D")
         p =  (pad <||> ped) <> swap "D" ("A", "E")
+
+        isValidState = memoryBounds (Map.fromList [("A", 2)])
     in do
-       mapM_ (print . toList) $ toList $ applyStarPolicy @(Maybe ()) p []
+       mapM_ (print . toList) $ toList $ applyStarPolicy p []
+       print $ isJust $ applyStarPolicyWithValidity isValidState p []
 
