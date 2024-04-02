ac0a066d55983e5af708f66e1e4c7b809301b423
Pavel Chuprikov <pschuprikov@gmail.com>
2024-04-02 20:25:59 +0200

    document memoryBounds

--- src/BellKAT/Test.hs
+++ src/BellKAT/Test.hs
@@ -11,6 +11,15 @@ countQubitsAtLocation l = length . filter (hasLocation l . bellPair) . toList
 
 -- | produces a predicate over valid states from upper bounds on the number of qubits at certain
 -- locations. The state is valid if and only if /all/ the bounds are satisfied.
+--
+-- For instance no constraints mean \"always true\" predicate:
+--
+--  @'memoryBounds' [] = 'const' True@
+--
+-- To impose an upper bound @3@ on the number of qubits at location @\"A\"@ use (assuming
+-- 'OverloadedLists' and 'OverloadedStrings' extensions:
+--
+--  @'memoryBounds' [(\"A\", 3)]@
 memoryBounds 
     :: Ord tag 
     => Map Location Int -- ^ Upper bounds on the number of qubits at respective locations
